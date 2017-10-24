fac01669cf1162218a90d38dd60f46092a0a32cd
Polleps <polleps@gmail.com>
2017-10-24 15:30:57 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginscreenComponent } from '../app/components/loginscreen/loginscreen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginscreenComponent
   ],
   imports: [
     BrowserModule
