42708c943182b41621c6b154d7ebd615b73a3752
Polleps <polleps@gmail.com>
2017-11-07 11:44:18 +0100

    deze shit werkt niet echt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
-
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import { LoginscreenComponent } from '../app/components/loginscreen/loginscreen.component';
-import { AngularFireModule} from 'angularfire2';
-import { AngularFireDatabase} from 'angularfire2/database';
+
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyA8HhAUwcIIfuxm3Z8HjquZhbBOTbkLx-0',
@@ -15,7 +16,7 @@ export const firebaseConfig = {
   messagingSenderID: '911761469430'
 };
 
-NgModule({
+@NgModule({
   declarations: [
     AppComponent,
     LoginscreenComponent
@@ -24,7 +25,8 @@ NgModule({
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabase
+    AngularFireDatabase,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/loginscreen/loginscreen.component.ts
+++ src/app/components/loginscreen/loginscreen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database-deprecated";
 
+
 @Component({
   selector: 'app-loginscreen',
   templateUrl: './loginscreen.component.html',
