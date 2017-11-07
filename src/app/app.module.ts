f3d3407a80b59b929b3320f0a9665410a5afb5cd
Polleps <polleps@gmail.com>
2017-11-07 22:06:53 +0100

    Router toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginscreenComponent } from '../app/components/loginscreen/loginscreen.component';
 import { HardwarelistComponent } from '../app/components/hardwarelist/hardwarelist.component';
+import { AddhardwareComponent } from '../app/components/addhardware/addhardware.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LoginscreenComponent},
+  {path: 'list', component: HardwarelistComponent},
+  {path: 'toevoegen', component: AddhardwareComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginscreenComponent,
-    HardwarelistComponent
+    HardwarelistComponent,
+    AddhardwareComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
